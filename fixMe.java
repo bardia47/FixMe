0193499404eaec76b09e1ae260ea231dcd362347
bardia47 <46025237+bardia47@users.noreply.github.com>
2018-12-20 17:47:41 +0330

    Update fixMe.java

--- fixMe.java
+++ fixMe.java
@@ -1,36 +1,39 @@
-package myapplication;
-
 import java.awt.*;
-public class MyApplication {
+public class fixMe {
 static final String startStr="\n****start of the program****";
-final String endStr="\n****end of the program****";
-public void main(String []arg){
+static final String endStr="\n****end of the program****";
+public static void main(String [] args){
 		printMessage(startStr);
 		Square s=new Square(100);
-		System.out.println(s.computePerimeter());
+
+                System.out.println(s.computePerimeter());
 		System.out.println(s.computeSurface());
-		Point p1=new Point(10,10);
-		Point p2=new Point(10.1,10.2);
-		ColorPoint cp1=new ColorPoint(1,1);
-		ColorPoint cp2=new ColorPoint(1,1,new Color(12,34,56));
-		cp1=p1;
-		p1=cp1;
-	MyApplication myA=new MyApplication();
-		myA.printMessage(endStr);
-		myA.printMessage(myA.endStr);
+	
+		printMessage(endStr);
+		
 }//main Method
-private void printMessage(String str)
+public void paint(Graphics g) 
+{
+    ColorPoint cp1=new ColorPoint(1,1);
+		Color cp2= new Color(12,34,56);
+                g.setColor(Color.BLUE);
+g.drawLine(1,1,1,1);
+g.setColor(cp2);
+g.drawLine(10,10,10,10);
+}
+private static void printMessage(String str)
 {
 	System.out.println(str);
 }
+}
 ////////////////////////////////
 class Square {
-	public Square(int init_x){x=init_x;}
-	public int computeSurface(){return x**x;}
+private int x;
+    public Square(int init_x){x=init_x;}
+	public int computeSurface(){return x*x;}
 	public int computePerimeter(){return 4*x;}
-	private int x;
+
 }//square
-}//myApplication
 class Point{
 	public Point(int init_x,int init_y){
 		x=init_x;
@@ -45,7 +48,9 @@ class Point{
 class ColorPoint extends Point{
 	public ColorPoint(int init_x,int init_y){
 		super(init_x,init_y);
-		color=Color.Blue;
+        }
+       Color getc() 
+       {      return Color.BLUE;
 	}
-	Color color;
+	
 }//ColorPoint
